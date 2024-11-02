51a3be3e6d79c614687074f081463a82664f52fc
MD Mahir Faisal <mahir.faisal@koneksys.com>
2024-11-02 07:18:50 +0600

    send json for home

--- index.js
+++ index.js
@@ -205,7 +205,15 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send('Mobile bazar "API"   Here');
+  res.send([
+    {
+      userId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    },
+  ]);
 });
 
 app.listen(port, () => {
