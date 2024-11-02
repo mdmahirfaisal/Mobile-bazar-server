7afc247f659ca46fbed4ab94d80abfb626ef012f
MD Mahir Faisal <mahir.faisal@koneksys.com>
2024-11-02 23:14:30 +0600

    exported module

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
-app.use(helmet());
+// app.use(helmet());
 const cors = require("cors");
 require("dotenv").config();
-
 const port = process.env.PORT || 5000;
 // Configure specific directives (for CSP, etc.)
 app.use(
@@ -22,6 +21,7 @@ app.use(
 app.use(cors());
 app.use(express.json());
 
+console.log("helmet: ", helmet);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dt2b3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
@@ -205,4 +205,6 @@ app.listen(port, () => {
   console.log(` listening ${port}`);
 });
 
-module.exports = app;
+module.exports = (req, res) => {
+  app(req, res);
+};
