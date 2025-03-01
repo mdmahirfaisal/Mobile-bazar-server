d29c24df749fbbc5f688f1af3be49ae547b33d31
MD Mahir Faisal <mahir.faisal@koneksys.com>
2025-03-02 00:39:27 +0600

    updated vercel.json file

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const express = require('express')
-const helmet = require("helmet");
+const express = require("express");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
-// app.use(helmet());
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-// Configure specific directives (for CSP, etc.)
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: {
-//       contentSecurityPolicy: false,
-//     },
-//   })
-// );
-
 
 // middle ware Z
 app.use(cors());
@@ -29,7 +18,6 @@ app.use((req, res, next) => {
   );
   next();
 });
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dt2b3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri);
@@ -44,37 +32,7 @@ async function run() {
     const reviewCollection = database.collection("review");
     const usersCollection = database.collection("users");
 
-    // GET API Load all orders
-    app.get("/orders", async (req, res) => {
-      const cursor = ordersCollection.find({});
-      const orders = await cursor.toArray();
-      res.send(orders);
-    });
-
-    // GET API orders by specific user
-    app.get("/ordersData", async (req, res) => {
-      const email = req.query.email;
-      const query = { email: email };
-      const cursor = ordersCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    // POST API  orders send to database
-    app.post("/orders", async (req, res) => {
-      const orders = req.body;
-      const result = await ordersCollection.insertOne(orders);
-      res.send(result);
-    });
-
-    // DELETE Order  with user
-    app.delete("/orders/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await ordersCollection.deleteOne(query);
-      res.send(result);
-    });
-
+  
     // GET API Load all products
     app.get("/products", async (req, res) => {
       const cursor = productsCollection.find({});
@@ -123,111 +81,11 @@ async function run() {
       res.json(result);
     });
 
-    // POST API  review send to database
-    app.post("/review", async (req, res) => {
-      const review = req.body;
-      const result = await reviewCollection.insertOne(review);
-      res.send(result);
-    });
-
-    // GET API Load all review
-    app.get("/review", async (req, res) => {
-      const cursor = reviewCollection.find({});
-      const review = await cursor.toArray();
-      res.send(review);
-    });
-
-    // GET API specific user email
-    app.get("/users/:email", async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
-      const user = await usersCollection.findOne(query);
-      let isAdmin = false;
-      if (user?.role === "admin") {
-        isAdmin = true;
-      }
-      res.json({ admin: isAdmin });
-    });
-
-    // POST API users
-    app.post("/users", async (req, res) => {
-      const user = req.body;
-      const result = await usersCollection.insertOne(user);
-      res.json(result);
-    });
-
-    // PUT API users
-    app.put("/users", async (req, res) => {
-      const user = req.body;
-      const filter = { email: user.email };
-      const options = { upsert: true };
-      const updateDoc = { $set: user };
-      const result = await usersCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      res.json(result);
-    });
-
-    // PUT API users admin
-    app.put("/users/admin", async (req, res) => {
-      const user = req.body;
-      const filter = { email: user.email };
-      const updateDoc = { $set: { role: "admin" } };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.json(result);
-    });
-
-    // PUT API status update
-    app.put("/updateOrderStatus", (req, res) => {
-      const { id, status } = req.body;
-      ordersCollection
-        .findOneAndUpdate(
-          { _id: ObjectId(id) },
-          {
-            $set: { status },
-          }
-        )
-        .then((result) => res.json(result.lastErrorObject.updatedExisting));
-    });
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send([
-    {
-      userId: 1,
-      id: 1,
-      title:
-        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    },
-  ]);
-});
-
-// GET API Load all products
-app.get("/product", async (req, res) => {
-  res.send([
-    {
-      userId: 1,
-      id: 1,
-      title:
-        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    },
-    {
-      userId: 2,
-      id: 1,
-      title:
-        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-    },
-  ]);
-});
 
 app.listen(port, () => {
   console.log(` listening ${port}`);
